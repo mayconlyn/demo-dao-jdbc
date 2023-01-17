e03bd754489968cb6a81aca10ce07d5c9c92744e
Maycon Lyncoln <maycon.lyn@gmail.com>
2023-01-17 12:46:58 -0300

    findById Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,9 +11,8 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Department department = new Department(1, "Books");
-        Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.00, department);
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
 }
