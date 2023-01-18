31e2e404406cf2559c23f17a8b1086ace9e3182b
Maycon Lyncoln <maycon.lyn@gmail.com>
2023-01-18 10:01:55 -0300

    Seller Delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,10 +7,13 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println("===== Test 1: Seller findById =====");
@@ -30,15 +33,20 @@ public class Program {
             System.out.println(obj);
         }
 
-        System.out.println("\n===== Test 3: Seller Insert =====");
+        System.out.println("\n===== Test 4: Seller Insert =====");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New Id" + newSeller.getId());
 
-        System.out.println("\n===== Test 3: Seller Update =====");
+        System.out.println("\n===== Test 5: Seller Update =====");
         seller = sellerDao.findById(1);
         seller.setName("Maycon Garcia");
         sellerDao.update(seller);
         System.out.println("Upadte Complete!");
+
+        System.out.println("\n===== Test 6: Seller Delete =====");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete Completed");
     }
 }
